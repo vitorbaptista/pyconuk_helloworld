43a07a310e4e1b7a4d200587824075ea306c8395
Vitor Baptista <vitor@vitorbaptista.com>
2016-09-15 13:55:17 +0100

    Add setuptools classifiers

--- setup.py
+++ setup.py
@@ -22,4 +22,16 @@ setup(
     author_email='vitor@vitorbaptista.com',
     license='MIT',
     packages=find_packages(exclude='tests'),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
 )
