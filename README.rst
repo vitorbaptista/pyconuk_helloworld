5f16a44a051b58c6001aaa3c5bf7edff02758b16
Vitor Baptista <vitor@vitorbaptista.com>
2016-09-15 13:54:26 +0100

    Add README

---
+++ README.rst
@@ -0,0 +1,12 @@
+Hello World
+===========
+
+Sample package that simply returns "Hello world". Written as part of the
+"Python Library Development" talk in PyConUK 2016.
+
+Installing
+----------
+
+.. code-block:: shell
+
+    $ pip install pyconuk_helloworld
--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
+import os
+import codecs
 from setuptools import setup, find_packages
+
+
+def read(*parts):
+    '''
+    Build an absolute path from *parts* and and return the contents of the
+    resulting file.  Assume UTF-8 encoding.
+    '''
+    BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(BASE_PATH, *parts), 'rb', 'utf-8') as f:
+        return f.read()
 
 setup(
     name='pyconuk_helloworld',
     version='0.0.1',
     description='A hello world package',
+    long_description=read('README.rst'),
     url='https://github.com/vitorbaptista/helloworld',
     author='Vitor Baptista',
     author_email='vitor@vitorbaptista.com',
